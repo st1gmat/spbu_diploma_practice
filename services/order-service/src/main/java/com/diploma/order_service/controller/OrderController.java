d55634e052bfe5d3bd99f368ded44a45cbc47382
Ilya Savchenko <savchenckoilia@gmail.com>
2025-02-26 16:05:30 +0300

    added finaAll & findById endpoints

--- services/order-service/src/main/java/com/diploma/order_service/controller/OrderController.java
+++ services/order-service/src/main/java/com/diploma/order_service/controller/OrderController.java
@@ -4,14 +4,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.diploma.order_service.models.order.OrderRequest;
+import com.diploma.order_service.models.order.OrderResponse;
 import com.diploma.order_service.services.OrderService;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
+import java.util.List;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 
 @RestController
@@ -24,6 +29,17 @@ public class OrderController {
     public ResponseEntity<Integer> createOrder(@RequestBody @Valid OrderRequest request) {
         return ResponseEntity.ok(service.createOrder(request));
     }
+
+    @GetMapping
+    public ResponseEntity<List<OrderResponse>> findAll() {
+        return ResponseEntity.ok(service.findAll());
+    }
+    
+    @GetMapping("/{order-id}")
+    public ResponseEntity<OrderResponse> findById(@PathVariable("order-id") Integer orderId) {
+        return ResponseEntity.ok(service.findById(orderId));
+    }
+    
     
 
 }
