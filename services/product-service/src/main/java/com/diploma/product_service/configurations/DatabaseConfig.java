a6a03d18f23572e5384c411d48df22b615ea35b0
Ilya Savchenko <savchenckoilia@gmail.com>
2025-03-12 20:48:57 +0300

    created flyway configuration

---
+++ services/product-service/src/main/java/com/diploma/product_service/configurations/DatabaseConfig.java
@@ -0,0 +1,26 @@
+package com.diploma.product_service.configurations;
+
+import org.flywaydb.core.Flyway;
+import org.springframework.boot.autoconfigure.flyway.FlywayProperties;
+import org.springframework.boot.autoconfigure.r2dbc.R2dbcProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableConfigurationProperties({R2dbcProperties.class, FlywayProperties.class})
+public class DatabaseConfig {
+
+    @Bean(initMethod = "migrate")
+    public Flyway flyway(FlywayProperties flywayProperties, R2dbcProperties r2dbcProperties) {
+        return Flyway.configure()
+            .dataSource(
+                flywayProperties.getUrl(),
+                flywayProperties.getUser(),   // root
+                flywayProperties.getPassword()    // root
+            )
+            .locations(flywayProperties.getLocations().stream().toArray(String[]::new))
+            .baselineOnMigrate(true)
+            .load();
+    }
+}
