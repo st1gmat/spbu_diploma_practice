97d86d355d4a2047994d6e5845f5eff6af542820
Ilya Savchenko <savchenckoilia@gmail.com>
2025-03-14 20:34:19 +0300

    added annotation to enable reactive mongodb repo

--- services/customer-service/src/main/java/com/diploma/customer_service/CustomerServiceApplication.java
+++ services/customer-service/src/main/java/com/diploma/customer_service/CustomerServiceApplication.java
@@ -2,8 +2,10 @@ package com.diploma.customer_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
 
 @SpringBootApplication
+@EnableReactiveMongoRepositories
 public class CustomerServiceApplication {
 
 	public static void main(String[] args) {
