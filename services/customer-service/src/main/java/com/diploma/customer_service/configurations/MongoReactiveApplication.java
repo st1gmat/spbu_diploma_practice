ca60ee23fa2a94088d7ace75994d1f0358b891b7
Ilya Savchenko <savchenckoilia@gmail.com>
2025-03-14 19:22:26 +0300

    created mongo reactive configuration module

---
+++ services/customer-service/src/main/java/com/diploma/customer_service/configurations/MongoReactiveApplication.java
@@ -0,0 +1,24 @@
+package com.diploma.customer_service.configurations;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.config.AbstractReactiveMongoConfiguration;
+import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
+
+import com.mongodb.reactivestreams.client.MongoClient;
+import com.mongodb.reactivestreams.client.MongoClients;
+
+@Configuration
+@EnableReactiveMongoRepositories
+public class MongoReactiveApplication extends AbstractReactiveMongoConfiguration {
+
+    @Bean
+    public MongoClient mongoClient() {
+        return MongoClients.create();
+    }
+
+    @Override
+    protected String getDatabaseName() {
+        return "customer_db";
+    }
+}
