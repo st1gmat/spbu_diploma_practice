3854081b53969b48a2deaed5028d5daac5818a4f
Ilya Savchenko <savchenckoilia@gmail.com>
2025-03-19 11:11:20 +0300

    removed async completablefuture & added smaller sleep for threads

--- services/notification-service/src/main/java/com/diploma/notification_service/services/EmailService.java
+++ services/notification-service/src/main/java/com/diploma/notification_service/services/EmailService.java
@@ -15,30 +15,30 @@ import java.util.concurrent.TimeUnit;
 public class EmailService {
 
     public void sendPaymentSuccessEmail(String recipient, String customerName, BigDecimal amount, String orderReference) {
-        CompletableFuture.runAsync(() -> {
+        // CompletableFuture.runAsync(() -> {
             try {
                 log.info("Simulating email sending... to {} (Payment Confirmation)", recipient);
-                TimeUnit.SECONDS.sleep(3); // Имитация задержки в 3 секунды
+                TimeUnit.MILLISECONDS.sleep(700); // Имитация задержки в 700 мс
                 log.info("Pseudo-email sent to: {} | Subject: Payment Confirmation | Body: Dear {}, your payment of ${} for order {} was successful!",
                         recipient, customerName, amount, orderReference);
             } catch (InterruptedException e) {
                 Thread.currentThread().interrupt();
                 log.error("Email sending was interrupted", e);
             }
-        });
+        // });
     }
 
     public void sendOrderConfirmationEmail(String recipient, String customerName, BigDecimal totalAmount, String orderReference, List<Product> products) {
-        CompletableFuture.runAsync(() -> {
+        // CompletableFuture.runAsync(() -> {
             try {
                 log.info("Simulating email sending... to {} (Order Confirmation)", recipient);
-                TimeUnit.SECONDS.sleep(3); // Имитация задержки в 3 секунды
+                TimeUnit.MILLISECONDS.sleep(700); // Имитация задержки в 700 мс
                 log.info("Pseudo-email sent to: {} | Subject: Order Confirmation | Body: Dear {}, your order {} with total amount ${} has been placed successfully! Products: {}",
                         recipient, customerName, orderReference, totalAmount, products);
             } catch (InterruptedException e) {
                 Thread.currentThread().interrupt();
                 log.error("Email sending was interrupted", e);
             }
-        });
+        // });
     }
 }
